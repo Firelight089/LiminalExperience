704538ea571fe284c36b9eacca0b5beb5e6ce96b
WhiteRavensGame <cuecharles@gmail.com>
2021-11-25 16:57:05 -0800

    FPS display prefab and script implemented

---
+++ SunriseExperience/Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FPSDisplay : MonoBehaviour
+{
+    public Text fpsText;
+    public float refreshTime = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateFPS", refreshTime, 1);
+    }
+
+    // Update is called once per frame
+    void UpdateFPS()
+    {
+        float fps = Mathf.RoundToInt(1 / Time.unscaledDeltaTime);
+        fpsText.text = fps.ToString();
+    }
+}
